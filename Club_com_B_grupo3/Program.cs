823bc35ff174d9e0e66cec61b5d1604b702caa78
guillermo_k <guillermo.fullstack@gmail.com>
2023-11-15 19:06:04 -0300

    Se agrego tabla noSocio, y el procedure correspondiente

--- Club_com_B_grupo3/Program.cs
+++ Club_com_B_grupo3/Program.cs
@@ -31,6 +31,8 @@ namespace Club_com_B_grupo3
                 MySqlCommand comandoBase = new MySqlCommand(creaBase, sqlCon);
                 string socio = "CREATE TABLE IF NOT EXISTS socio(nombre varchar(50),apellido varchar(50),direccion varchar(50),mail varchar(50),telefono varchar(20),dni int PRIMARY KEY,apto boolean);";
                 MySqlCommand comandoSocio = new MySqlCommand(socio, sqlCon);
+                string noSocio = "CREATE TABLE IF NOT EXISTS nosocio(nombre varchar(50),apellido varchar(50),direccion varchar(50),mail varchar(50),telefono varchar(20),dni int PRIMARY KEY,apto boolean);";
+                MySqlCommand comandoNoSocio = new MySqlCommand(noSocio, sqlCon);
                 string roles = "CREATE TABLE IF NOT EXISTS roles(RolUsu int PRIMARY KEY,NomRol varchar(30));";
                 MySqlCommand comandoRoles = new MySqlCommand(roles, sqlCon);
                 string usuario = "CREATE TABLE IF NOT EXISTS usuario(nombre varchar(50) PRIMARY KEY,passw varchar(50),activo boolean,rol int,FOREIGN KEY (rol) REFERENCES roles(RolUsu));";
@@ -41,19 +43,24 @@ namespace Club_com_B_grupo3
                 MySqlCommand comandoCargaAdmin = new MySqlCommand(cargaAdmin, sqlCon);
                 string procIngSocio = "CREATE procedure ingresarSocio(IN nom varchar(50), IN ape varchar(50), IN dir varchar(50), IN mail varchar(50), IN tel varchar(20), IN pdni int, IN apto boolean, OUT exito boolean) BEGIN declare existe int; set exito = false; set existe = (SELECT count(*) FROM socio WHERE dni LIKE pdni);  IF  existe < 1 THEN INSERT INTO socio VALUES(nom, ape, dir, mail, tel, pdni, apto); set exito = True; END IF; END ";
                 MySqlScript comandoProcIngSocio = new MySqlScript(sqlCon, procIngSocio);
+                string procIngNoSocio = "CREATE procedure ingresarNoSocio(IN nom varchar(50), IN ape varchar(50), IN dir varchar(50), IN mail varchar(50), IN tel varchar(20), IN pdni int, IN apto boolean, OUT exito boolean) BEGIN declare existe int; set exito = false; set existe = (SELECT count(*) FROM nosocio WHERE dni LIKE pdni);  IF  existe < 1 THEN INSERT INTO nosocio VALUES(nom, ape, dir, mail, tel, pdni, apto); set exito = True; END IF; END ";
+                MySqlScript comandoProcIngNoSocio = new MySqlScript(sqlCon, procIngNoSocio);
                 string procLogin = "CREATE procedure IngresoLogin(in Usu varchar(20),in Pass varchar(15)) BEGIN SELECT NomRol FROM usuario INNER JOIN roles ON Rol = RolUsu WHERE nombre = Usu AND passw = Pass  AND Activo = 1; END  //";
                 MySqlScript comandoProcLogin = new MySqlScript(sqlCon, procLogin);
                 comandoProcLogin.Delimiter = "//";
                 comandoProcIngSocio.Delimiter = "//";
+                comandoProcIngNoSocio.Delimiter = "//";
 
 
                 comandoBase.ExecuteNonQuery();
                 comandoSocio.ExecuteNonQuery();
+                comandoNoSocio.ExecuteNonQuery();
                 comandoRoles.ExecuteNonQuery();
                 comandoUsuario.ExecuteNonQuery();
                 comandoIngresoRoles.ExecuteNonQuery();
                 comandoCargaAdmin.ExecuteNonQuery();
                 comandoProcIngSocio.Execute();
+                comandoProcIngNoSocio.Execute();
                 comandoProcLogin.Execute();
             }
             finally
